6ef099f6dcf5d784ad60de75bc20c1605f2cf30b
deepakjnv880 <b17039@students.iitmandi.ac.in>
2019-10-25 10:42:52 +0530

    Redo name change to undo

--- prog1.py
+++ prog1.py
@@ -65,7 +65,7 @@ for i in range(1,6):
         z = z + 1
 
 Label(master, text='\n').grid(row=7)
-button = Button(master, text='Redo',activeforeground='blue',activebackground='red', command=lambda: r.redo_in_excel_sheet(cr_vars))
+button = Button(master, text='Undo',activeforeground='blue',activebackground='red', command=lambda: r.redo_in_excel_sheet(cr_vars))
 button.grid(row=8,column=3)
 button = Button(master, text='Save',activeforeground='blue',activebackground='red', command=lambda: s.save_in_excel_sheet(old_var))
 button.grid(row=8,column=5)
