b827b3ffd438a26c3875abac6352047280b90353
Roshan Sharma <rsharma21097@gmail.com>
2019-11-15 15:23:32 -0500

    Fully Final

--- progs/func/read_create.py
+++ progs/func/read_create.py
@@ -22,7 +22,7 @@ def read_create():
             for p in range(len(row)):
                 if(row[p]=='C'):    
                     name[p] =name[p].replace("/", "-")
-                    if(os.path.exists("/src/tmp/baskets/"+str(name[p]) + ".xlsx")  ):
+                    if(os.path.exists("src/tmp/baskets/"+str(name[p]) + ".xlsx")  ):
                         wb = openpyxl.load_workbook("src/tmp/baskets/"+str(name[p]) + ".xlsx")
                         pass
                     else:
@@ -31,6 +31,7 @@ def read_create():
                         wb = openpyxl.load_workbook("src/tmp/baskets/"+str(name[p]) + ".xlsx")
                     ws = wb.worksheets[0]
                     rows = ws.max_row+1
+                    print(rows,row)
                     for l in range(1,6):
                         ws.cell(rows,l).value=row[l]
                         wb.save("src/tmp/baskets/"+str(name[p]) + ".xlsx")
